4a9c0ad613284870060baac169b48f2642b4c515
reistence <gvlamberti@gmail.com>
2024-01-20 20:07:43 +0100

    y

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,62 +1,28 @@
-
-uniform float progress;
 uniform float time;
-uniform sampler2D uTexture1;
-uniform sampler2D uTexture2;
+uniform float progress;
+uniform sampler2D uTexture;
+uniform sampler2D uDisplacement;
 uniform vec4 resolution;
+
+
 varying vec2 vUv;
-varying vec3 position;
+varying vec3 vPositions;
 float PI = 3.141592653589793238;
 
-vec2 mirrored(vec2 v) {
-  vec2 m = mod(v, 2.);
-  return mix(m, 2.0 - m, step(1.0, m));
 
-}
 
-float tri(float p ){
-  return mix(p, 1.0 - p, step(0.5, p))* 2.;
-}
 
 
 void main() {
-  vec4 t = texture2D(uTexture1, vUv);
-  vec4 t1 = texture2D(uTexture2, vUv);
-  float sweep = step( vUv.y, progress);
+  vec4 displacement = texture2D(uDisplacement, vUv);
 
-  vec4 finalTexture = mix(t, t1, sweep);
-  // gl_FragColor = t * progress;
-  gl_FragColor = finalTexture;
+  float theta = displacement.r * 2.0 * PI;
 
+  vec2 dir = vec2(sin(theta), cos(theta));
 
-  float p = progress;
-  vec2 vUv1 = vUv;
-  float delayValue = p * 7. - vUv.y * 2. + vUv.x - 2.;
+  vec2 uv = vUv + dir * displacement.r * 0.1; 
 
-  delayValue = clamp(delayValue, 0., 1.);
+  vec4 color = texture2D(uTexture, uv);
 
-  float accel = 0.1;
-
-  vec2 translateValue = vec2(p) + delayValue * accel;
-  vec2 translateValue1 = vec2(-0.5, 1.) * translateValue;
-  vec2 translateValue2 = vec2(-0.5, 1.) * (translateValue - 1. - accel);
-
-  vec2 w = sin( sin(time) * vec2(0, 0.3) + vUv.yx * vec2(0, 4.)) * vec2(0, 0.5);
-
-  vec2 xy = w * ( tri(p) * 0.5 + tri(delayValue) * 0.5 );
-
-  vec2 uv1 = vUv1 + translateValue1 + xy;
-  vec2 uv2 = vUv1 + translateValue2 + xy;
-
-  vec4 rgba1 = texture2D(uTexture1, mirrored(uv1));
-  vec4 rgba2 = texture2D(uTexture1, mirrored(uv2));
-
-  vec4 rgba = mix(rgba1, rgba2, delayValue);
-
-  gl_FragColor = rgba;
-
-
-
-
-
+  gl_FragColor = color;
 }
